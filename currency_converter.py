230cd08557ac8379bf70e8e270a1ce371098334a
linguin0 <148582087+linguin0@users.noreply.github.com>
2024-10-28 19:27:10 +0000

    Complete

---
+++ currency_converter.py
@@ -0,0 +1,18 @@
+
+def currency_converter(amount, from_currency, to_currency):
+    
+    conversion_rate = {
+    'GBP': {'CNY': 9.24, 'PHP': 75.00, 'INR': 109.06},
+    'CNY': {'GBP': 0.11, 'PHP': 8.12, 'INR': 11.81},
+    'PHP': {'CNY': 0.12, 'GBP': 0.01, 'INR': 1.45},
+    'INR': {'PHP': 0.69, 'CNY': 0.09, 'GBP': 0.01}
+    }
+
+    values = conversion_rate[from_currency]
+    rate = values[to_currency]
+
+    conversion = amount * rate
+    return conversion
+
+# Submit only this file currency_converter.py with the completed function.
+# Do not add additional code for calling the function, or code to get user input.

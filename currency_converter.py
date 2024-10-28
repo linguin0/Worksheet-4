5f586642b79f7b2076cfcf75b3a81ae22daf83cd
linguin0 <148582087+linguin0@users.noreply.github.com>
2024-10-28 19:29:13 +0000

    Complete

--- currency_converter.py
+++ currency_converter.py
@@ -1,18 +1,19 @@
 
 def currency_converter(amount, from_currency, to_currency):
-    
-    conversion_rate = {
-    'GBP': {'CNY': 9.24, 'PHP': 75.00, 'INR': 109.06},
-    'CNY': {'GBP': 0.11, 'PHP': 8.12, 'INR': 11.81},
-    'PHP': {'CNY': 0.12, 'GBP': 0.01, 'INR': 1.45},
-    'INR': {'PHP': 0.69, 'CNY': 0.09, 'GBP': 0.01}
-    }
+    if amount > 0:
+        conversion_rate = {
+        'GBP': {'CNY': 9.24, 'PHP': 75.00, 'INR': 109.06},
+        'CNY': {'GBP': 0.11, 'PHP': 8.12, 'INR': 11.81},
+        'PHP': {'CNY': 0.12, 'GBP': 0.01, 'INR': 1.45},
+        'INR': {'PHP': 0.69, 'CNY': 0.09, 'GBP': 0.01}
+        }
 
-    values = conversion_rate[from_currency]
-    rate = values[to_currency]
+        values = conversion_rate[from_currency]
+        rate = values[to_currency]
 
-    conversion = amount * rate
-    return conversion
+        conversion = amount * rate
+        return conversion
+    return 0.0
 
 # Submit only this file currency_converter.py with the completed function.
 # Do not add additional code for calling the function, or code to get user input.
